3bac02819fafae0b7971d11e0ae8a48d98127db0
James Klee <james.klee@outlook.com>
2015-08-20 19:34:38 +0100

    Added vim and tmux config files

--- Tup_Test/hello.cpp
+++ Tup_Test/hello.cpp
@@ -8,61 +8,62 @@ int main()
    	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!", sf::Style::Fullscreen, settings);
 	window.setVerticalSyncEnabled(true);
 	sf::CircleShape shape(100.f, 4);
-    	shape.setFillColor(sf::Color::Green);
+   	shape.setFillColor(sf::Color::Green);
 
 	sf::View view(sf::FloatRect(0, 0, window.getSize().x/4, window.getSize().y/4));
 	window.setView(view);
 
 	float cameraSpeed = 25.0f;
-	
 	sf::Clock clock;
     	while (window.isOpen())
     	{
         	sf::Event event;
         	while (window.pollEvent(event))
         	{
-        	    
-			switch (event.type) {
-				case (sf::Event::Closed):
-        	        		window.close();
-					break;
+				switch (event.type) {
+					case (sf::Event::Closed):
+								window.close();
+						break;
 
-				case (sf::Event::KeyPressed):
+					case (sf::Event::KeyPressed):
 					
-					switch(event.key.code) {
-						case (sf::Keyboard::Escape):
-							window.close();
-							break;
+						switch(event.key.code) {
+							case (sf::Keyboard::Escape):
+								window.close();
+								break;
 						
-						default:
-							break;
-					}						
-					break;				
+							default:
+								break;
+						}						
+						break;				
 
-				default:
-					break;
+					default:
+						break;
+				}
+        	}	
+		
+			if (true) {
+				
+			}	
+
+			float moveViewX = 0.0f;
+			float moveViewY = 0.0f;
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+				moveViewX = -cameraSpeed;		
+			} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) { 
+				moveViewX = cameraSpeed;
+			} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) { 
+				moveViewY = -cameraSpeed;
+			} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { 
+				moveViewY = cameraSpeed;
 			}
-        	}
-		
-		
-		float moveViewX = 0.0f;
-		float moveViewY = 0.0f;	
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-			moveViewX = -cameraSpeed;		
-		} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) { 
-			moveViewX = cameraSpeed;
-		} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) { 
-			moveViewY = -cameraSpeed;
-		} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { 
-			moveViewY = cameraSpeed;
-		}
+			window.clear();
 
-        	window.clear();
-
-		sf::Time elapsedTime = clock.restart();
-		view.move(moveViewX * elapsedTime.asSeconds(), moveViewY * elapsedTime.asSeconds());
-		window.setView(view);
+			sf::Time elapsedTime = clock.restart();
+			view.move(moveViewX * elapsedTime.asSeconds(), moveViewY * elapsedTime.asSeconds());
+			window.setView(view);
         	window.draw(shape);
         	window.display();
     	}
